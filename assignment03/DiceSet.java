c0a6f5c2e9ac56ab111c43fbb73cec98a274105f
Pierce Bartolo <PierceBartolo@MacBook-Air-4.local>
2020-02-17 22:14:27 -0800

    updated test main for DiceSet.java

--- assignment03/DiceSet.java
+++ assignment03/DiceSet.java
@@ -155,10 +155,68 @@ public class DiceSet {
 
       System.out.println("Testing Collective Roll of ds2");
       ds2.roll();
-      System.out.println("New numbers after Collective roll are " + ds2.toString());
+      System.out.println("New numbers after Collective roll are: " + ds2.toString());
 
-      System.out.println("Testing the Sum Method");
-      System.out.println("Sum of the Diceset is " + ds2.sum());
+      System.out.println("Testing the Sum Method for ds2");
+      System.out.println("Sum of the Diceset is: " + ds2.sum());
+
+      System.out.println("Testing the Roll Indiviual Method for ds2");
+      try{
+        ds2.rollIndividual(3);
+        System.out.println("Value after individual roll is: " + ds2.toString());
+        System.out.println("Testing sum after individual roll, the new sum is: " + ds2.sum());
+      } catch(IllegalArgumentException iae) {
+        System.out.println("Index is invalid");
+      }
+
+      System.out.println("Testing to get Indivual Die Value in ds2");
+      try{
+        System.out.println("Value of first die (index 0): " + ds2.getIndividual(0));
+        System.out.println("Value of second die (index 1): " + ds2.getIndividual(1));
+        System.out.println("Value of third die (index 2): " + ds2.getIndividual(2));
+        System.out.println("Value of fourth die (index 3): " + ds2.getIndividual(3));
+      } catch(IllegalArgumentException iae) {
+        System.out.println("index is out of range");
+      }
+
+      System.out.println("Second Round of Testing with DiceSet ds3");
+      DiceSet ds3 = new DiceSet(5, 8);
+      try{
+        System.out.println("Successfully created ds3 of count 5 and sides 8");
+        System.out.println(ds3.toString());
+      } catch(IllegalArgumentException iae) {
+        System.out.println("No DiceSet created, can't make a DiceSet of 0 die");
+      }
+
+      System.out.println("Testing Collective Roll of ds3");
+      ds3.roll();
+      System.out.println("New value of ds3 after rolling all die are: " + ds3.toString());
+
+      System.out.println("Testing the Sum of All Dice Values in ds3");
+      System.out.println("The sum of the DiceSet is: " + ds3.sum());
+
+      System.out.println("Testing Indiviual Roll Method for ds3");
+      try {
+       ds3.rollIndividual(2);
+      System.out.println("Value after indivdual roll is: " + ds3.toString());
+      ds3.rollIndividual(4);
+      System.out.println("Value after individual roll is: " + ds3.toString());
+      System.out.println("Testing sum() after indiviual roll, new sum of set is: " + ds3.sum()); 
+      } catch(IllegalArgumentException iae) {
+        System.out.println("Index is out of range");
+      }
+
+      System.out.println("Testing to get Indiviual Die Value in ds3");
+      try{
+        System.out.println("Value of first die (index 0): " + ds3.getIndividual(0));
+        System.out.println("Value of second die (index 1): " + ds3.getIndividual(1));
+        System.out.println("Value of third die (index 2): " + ds3.getIndividual(2));
+        System.out.println("Value of fourth die (index 3): " + ds3.getIndividual(3));
+        System.out.println("Value of fifth die (index 4): " + ds3.getIndividual(4));
+      } catch(IllegalArgumentException iae) {
+        System.out.println("Index is out of range");
+      }
+      
       }
 
    }
