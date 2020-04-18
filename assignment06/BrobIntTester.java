095653e93123ced8a62dc135a6b3c67af5a68911
Pierce Bartolo <PierceBartolo@MacBook-Air-4.attlocal.net>
2020-04-17 20:24:58 -0700

    Set up BrobIntTester and BrobInt template

---
+++ assignment06/BrobIntTester.java
@@ -0,0 +1,94 @@
+public class BrobIntTester{
+
+
+   private static String g01String = "144127909719710664015092431502440849849506284148982076191826176553";
+   private static String g02String = "144127909719710664015092431502440849849506284148982076191826176553";
+   private static String g03String = "144127909719710664015092431502440849849506284108982076191826176553";
+   private static String g04String = "14412790971971066401509243150244084984950628410898207";
+   private static String g05String = "0";
+   private static String g06String = "1";
+   private static String g07String = "10";
+   private static String g11String = "10";
+   private static String g12String = "20";
+   private static String g13String = "234567";
+   private static String g14String = "-234567";
+   private static String g15String = "-10";
+   private static String g16String = "-999999";
+   private static String g17String = "765";
+   private static String g18String = "23";
+   private static String g19String = "56789";
+   private static String g20String = "37";
+   private static String g21String = "00000";
+   private static String g22String = "$%#@";
+   private static String g23String = "45342332.000";
+   private static String g24String = "0.23424325";
+   private static String g25String = "";
+   private static String g26String = "Good day";
+   private static String g27String = "+185";
+   private static String g28String = "18+5";
+   private static String g29String = "44";
+   private static String g30String = "%4";
+   private static String g31String = "00";
+   private static String g32String = "100000000";
+   private static String g33String = "1ooo2300";
+   private static String g34String = "1!@#4%5";
+   private static String g35String = "8989";
+   private static String g36String = ":990";
+   private static String g37String = "00123";
+   private static String g38String = "4-67";
+   private static String g39String = "2";
+   private static String g40String = "435";
+
+   private static BrobInt g1 = null;
+   private static BrobInt g2 = null;
+   private static BrobInt g3 = null;
+   private static BrobInt g4 = null;
+   private static BrobInt g5 = null;
+   private static BrobInt g6 = null;
+   private static BrobInt g7 = null;
+   private static BrobInt g8 = null;
+   private static BrobInt g9 = null;
+   private static BrobInt g10 = null;
+   private static BrobInt g11 = null;
+   private static BrobInt g12 = null;
+   private static BrobInt g13 = null;
+   private static BrobInt g14 = null;
+   private static BrobInt g15 = null;
+   private static BrobInt g16 = null;
+   private static BrobInt g17 = null;
+   private static BrobInt g18 = null;
+   private static BrobInt g19 = null;
+   private static BrobInt g20 = null;
+   private static BrobInt g21 = null;
+   private static BrobInt g22 = null;
+   private static BrobInt g23 = null;
+   private static BrobInt g24 = null;
+   private static BrobInt g25 = null;
+   private static BrobInt g26 = null;
+   private static BrobInt g27 = null;
+   private static BrobInt g29 = null;
+   private static BrobInt g30 = null;
+   private static BrobInt g31 = null;
+   private static BrobInt g32 = null;
+   private static BrobInt g33 = null;
+   private static BrobInt g34 = null;
+   private static BrobInt g35 = null;
+   private static BrobInt g36 = null;
+   private static BrobInt g37 = null;
+   private static BrobInt g38 = null;
+   private static BrobInt g39 = null;
+   private static BrobInt g40 = null;
+
+   public BrobIntTester() {
+
+   }
+
+   public static void main(String[] args) {
+   	BrobIntTester git = new BrobIntTester();
+
+   	System.out.println("\n Hello, world, from the BrobInt program!");
+
+  System.out.println( "    TESTING CONSTRUCTOR AND CONSTANTS:\n" +
+                          "    ==================================" );
+   }
+}
